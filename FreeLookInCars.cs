f097bec9ec062d6db515fe84b7d0a67c36ab32fb
WulfMarius <WulfMarius@users.noreply.github.com>
2017-08-12 21:14:15 +0200

    initial version

---
+++ FreeLookInCars.cs
@@ -0,0 +1,15 @@
+﻿using Harmony;
+using UnityEngine;
+
+namespace FreeLookInCars
+{
+    [HarmonyPatch(typeof(PlayerInVehicle))]
+    [HarmonyPatch("Start")]
+    public class FreeLookInCars
+    {
+        public static void Postfix(PlayerInVehicle __instance)
+        {
+            __instance.m_YawLimitDegrees = new Vector2(-175, 175);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+FreeLookInCars
+
+
+This is a mod for **The Long Dark** by Hinterland Games Studio, Inc.
+
+
+It allows to look out the back window in cars by increasing the horizontal movement range.
+
+
+Requires the [Mod Loader](https://github.com/zeobviouslyfakeacc/ModLoaderInstaller) to run.

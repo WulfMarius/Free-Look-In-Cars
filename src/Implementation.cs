81623a835774cc4a3f1bf45c78a7ffa0e7552cb3
WulfMarius <WulfMarius@users.noreply.github.com>
2018-12-19 14:57:47 +0100

    updated to work with TLD v1.41

---
+++ src/Implementation.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using UnityEngine;
+
+namespace FreeLookInCars
+{
+    public class Implementation
+    {
+        public const string NAME = "Free-Look-In-Cars";
+
+        public static void OnLoad()
+        {
+            AssemblyName assemblyName = Assembly.GetExecutingAssembly().GetName();
+            Log("Version " + assemblyName.Version);
+        }
+
+        internal static void Log(string message)
+        {
+            Debug.LogFormat("[" + NAME + "] {0}", message);
+        }
+
+        internal static void Log(string message, params object[] parameters)
+        {
+            string preformattedMessage = string.Format("[" + NAME + "] {0}", message);
+            Debug.LogFormat(preformattedMessage, parameters);
+        }
+    }
+}
